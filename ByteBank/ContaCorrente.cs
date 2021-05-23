2f107e4c6833727556e9ad89f5d2ecac4a8b16af
Leandro Alcoforado <leandrofalcao@live.com>
2021-05-22 21:24:41 -0300

    primeiro commit do projeto

---
+++ ByteBank/ContaCorrente.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBan {
+    public class ContaCorrente {
+        public string titular;
+        public int agencia;
+        public int numero;
+        public double saldo;
+    }
+}
---
+++ ByteBank/Program.cs
@@ -0,0 +1,13 @@
+﻿using ByteBan;
+using System;
+
+namespace ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new ContaCorrente();
+        }
+    }
+}
