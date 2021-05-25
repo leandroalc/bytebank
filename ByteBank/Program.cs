d92e02db93a4b93b18eff931296aaaf6e82d9b78
Leandro Alcoforado <leandrofalcao@live.com>
2021-05-25 01:08:40 -0300

    testing first class

--- ByteBank/Program.cs
+++ ByteBank/Program.cs
@@ -7,7 +7,22 @@ namespace ByteBank
     {
         static void Main(string[] args)
         {
-            new ContaCorrente();
+            ContaCorrente contaDaGabriela = new ContaCorrente();
+
+            contaDaGabriela.titular = "Gabriela";
+            contaDaGabriela.agencia = 863;
+            contaDaGabriela.numero = 863452;
+            contaDaGabriela.saldo = 100;
+
+            Console.WriteLine("Titular: " + contaDaGabriela.titular);
+            Console.WriteLine("Agência: " + contaDaGabriela.agencia);
+            Console.WriteLine("Número: " + contaDaGabriela.numero);
+            Console.WriteLine("Saldo: " + contaDaGabriela.saldo);
+
+            contaDaGabriela.saldo += 200;
+            Console.WriteLine("Saldo: " + contaDaGabriela.saldo);
+
+            Console.ReadLine();
         }
     }
 }
